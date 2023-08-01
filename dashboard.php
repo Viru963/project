ba5fbbcd86ff00510c4cd2263008cb19e1f1da5b
vinayak21-singh <80423182+vinayak21-singh@users.noreply.github.com>
2023-08-01 12:36:15 +0530

    h

---
+++ dashboard.php
@@ -0,0 +1,66 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+   header('location:login_form.php');
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+   <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+<div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus'></i>
+      <span class="logo_name">SNTI internship project</span>
+    </div>
+      <ul class="nav-links">
+        <li>
+          <a href="#" class="active">
+            <i class='bx bx-grid-alt' ></i>
+            <span class="links_name">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class='bx bx-cog' ></i>
+            <span class="links_name">Setting</span>
+          </a>
+        </li>
+      </ul>
+  </div>
+  <section class="home-section">
+    <nav>
+      <div class="sidebar-button">
+        <i class='bx bx-menu sidebarBtn'></i>
+        <span class="dashboard">Dashboard</span>
+      </div>
+      <div class="search-box">
+        <input type="text" placeholder="Search...">
+        <i class='bx bx-search' ></i>
+      </div>
+      <div >
+        <img src="images/profile.jpg" alt="">
+        <b><span class="admin_name">Admin</span></b>
+        <br/>
+        <span><?php echo $_SESSION['admin_name'] ?></span>
+        <i class='bx bx-chevron-down' ></i>
+        <a href="logout.php" class="btn">logout</a>
+      </div>
+    </nav>
+          
+      </div>
+    </div>
+  </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>snti project</title>
+</head>
+<body>
+<nav>
+    <div class="navbar">
+        <img src="logo.jpg"class="hello">
+      <div class="logo"><a href="index.php">SNTI internship project</a></div>
+      <ul class="menu">
+        <li><a href="#Home">Home</a></li>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Services">Services</a></li>
+        <li><a href="#Contact">Contact us</a></li>
+        <li><a href="login_form.php">Login</a></li>
+        <li><a href="register_form.php">Sign up</a></li>
+      </ul>
+    </div>
+  </nav>
+  <section id="Home"></section>
+  <section id="About"></section>
+  <section id="Services"></section>
+  <section id="Contact"></section>
+  <div class="button">
+    <a href="#Home"><i class="fas fa-arrow-up"></i></a>
+  </div>
+</body>
+</html>
